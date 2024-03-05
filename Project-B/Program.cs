8412094233387fe0ad5d7f3b6d91ba82acd43d24
Bluefire0781 <andreskorevaar2@gmail.com>
2024-03-05 13:50:33 +0100

    Base file
    die base ding bog geadd

---
+++ Project-B/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program 
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.ReadLine();
+    }
+}
---
+++ Project-Btest/Project-Btest.cs
@@ -0,0 +1,11 @@
+namespace Project_Btest;
+
+[TestClass]
+public class Project_Btest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        
+    }
+}
