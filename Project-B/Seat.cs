3e8ad78d02271057dd0b5cdceae9389b7f11e18b
Jasper <1056722@hr.nl>
2024-03-19 13:15:44 +0100

    Added plane class and seat class
    basic additions

---
+++ Project-B/Seat.cs
@@ -0,0 +1,12 @@
+public class Seat
+{
+    public int ID;
+    public string Quality;
+    public int Price;
+
+    public Seat(int id, string quality)
+    {
+        ID = id;
+        Quality = quality;
+    }
+}
