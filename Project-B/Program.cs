a5fe617e548e6197a6363c7d59b2c6c4bd6ec323
Joey Zwinkels <joeyzwinkels@gmail.com>
2024-03-21 14:00:50 +0100

    New Program

--- Project-B/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Program 
-{
-    static void Main()
-    {
-        // Console.WriteLine("Hello, World!");
-        Mail.Mailsender("Joey", "joeyzwinkels@gmail.com", "24885645");
-    }
-}
