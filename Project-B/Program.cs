c0a61d38293bc537ced363f6e92612db7dfb8283
Joey Zwinkels <joeyzwinkels@gmail.com>
2024-03-21 13:42:39 +0100

    Added FlightDelete Method

--- Project-B/Program.cs
+++ Project-B/Program.cs
@@ -2,7 +2,6 @@
 {
     static void Main()
     {
-        // Console.WriteLine("Hello, World!");
-        Mail.Mailsender("Joey", "joeyzwinkels@gmail.com", "24885645");
+        Flightinfo.FlightDelete();
     }
 }
