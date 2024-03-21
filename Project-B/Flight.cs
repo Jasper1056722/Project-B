9b7e8f7eccf0c4af7e532c0b6f41361198a24fed
Jasper <1056722@hr.nl>
2024-03-21 14:29:34 +0100

    Flight class added

---
+++ Project-B/Flight.cs
@@ -0,0 +1,20 @@
+public class Flight
+{
+    public int FlightNumber;
+    public string Destination;
+    public string Country;
+    public Plane Airplane;
+    public string DepartingFrom;
+    public DateTime DepartureDate;
+    public DateTime EstimatedTimeofArrival;
+
+    public Flight(int flightNumber, string destination, string country, Plane airplane, string departingFrom, string departureDate, string estimatedTimeOfArrival)
+    {
+        FlightNumber = flightNumber;
+        Destination = destination;
+        Country = country;
+        Airplane = airplane;
+        DepartureDate = DateTime.ParseExact(departureDate, "dd-MM-yyyy HH:mm:ss", null);
+        EstimatedTimeofArrival = DateTime.ParseExact(estimatedTimeOfArrival, "dd-MM-yyyy HH:mm:ss", null);
+    }
+}
