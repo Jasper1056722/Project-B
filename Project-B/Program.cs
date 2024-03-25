feecd6dd7cb22cb330130acedbc425e23d69490a
Bluefire0781 <andreskorevaar2@gmail.com>
2024-03-25 15:19:00 +0100

    Logged in Bool + log in system

--- Project-B/Program.cs
+++ Project-B/Program.cs
@@ -1,11 +1,19 @@
-﻿using System.Data.SQLite;
+﻿
 public class Program 
 {
     static void Main()
     {
         // Console.WriteLine("Hello, World!");
         //Mail.Mailsender("Joey", "joeyzwinkels@gmail.com", "24885645");
-        Account.signup();
+        Account account = new Account();
+
+        //account.signup();
+        account.Login();
+        if (account.IsLoggedIn)
+        {
+            Console.WriteLine("petpet");
+            Console.WriteLine(account.Primkey);
+        } 
         Console.ReadKey();
     }
 }
