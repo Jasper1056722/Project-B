c91c9c614d748d57317ae621cdb2834841c14bf1
Jasper <1056722@hr.nl>
2024-03-27 18:34:25 +0100

    Laden en Writen naar de Json werkt volledig

--- Project-B/Flight.cs
+++ Project-B/Flight.cs
@@ -1,21 +1,55 @@
+using Newtonsoft.Json;
+
 public class Flight
 {
     public int FlightNumber { get; set; }
     public string Destination { get; set; }
     public string Country { get; set; }
-    public Plane Airplane { get; set; }
-    public string DepartingFrom { get; set; }
-    public DateTime DepartureDate { get; set; }
-    public DateTime EstimatedTimeofArrival { get; set; }
 
-    public Flight(int flightNumber, string destination, string country, Plane airplane, string departingFrom, string departureDate, string estimatedTimeOfArrival)
+    public string DepartingFrom { get; set; }
+    public string DepartureDate { get; set; }
+    public DateTime? DepartureTime { get; set; }
+    public DateTime? EstimatedTimeofArrival { get; set; }
+    public Plane Airplane { get; set; }
+
+    public Flight(int flightNumber, string destination, string country, Plane airplane, string departingFrom, string departureDate, string departureTime, string estimatedTimeOfArrival)
     {
         FlightNumber = flightNumber;
         Destination = destination;
         Country = country;
         Airplane = airplane;
         DepartingFrom = departingFrom;
-        DepartureDate = DateTime.ParseExact(departureDate, "dd-MM-yyyy HH:mm:ss", null);
-        EstimatedTimeofArrival = DateTime.ParseExact(estimatedTimeOfArrival, "dd-MM-yyyy HH:mm:ss", null);
+        DepartureDate = departureDate;
+
+        try
+        {
+            EstimatedTimeofArrival = DateTime.ParseExact(estimatedTimeOfArrival, "dd-MM-yyyy HH:mm:ss", null);
+        }
+        catch (FormatException)
+        {
+            EstimatedTimeofArrival = null;
+        }
+
+        try
+        {
+            DepartureTime = DateTime.ParseExact(departureTime, "dd-MM-yyyy HH:mm:ss", null);
+        }
+        catch (FormatException)
+        {
+            DepartureTime = null;
+        }
+    }
+
+    public static List<Flight> LoadJson()
+    {
+        string json = File.ReadAllText("Flights.json");
+        List<Flight> flights = JsonConvert.DeserializeObject<List<Flight>>(json);
+        return flights;
+    }
+
+    public static void WriteToJson(List<Flight> flights)
+    {
+        string json = JsonConvert.SerializeObject(flights, Formatting.Indented);
+        File.WriteAllText("Flights.json", json);
     }
 }
---
+++ Project-B/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Plane plane = new("Boeing 737");
+        List<Flight> flights = new List<Flight>
+        {
+
+        };
+        flights = Flight.LoadJson();
+
+        foreach(Flight flight in flights)
+        {
+            Console.WriteLine(flight.DepartingFrom);
+        }
+
+        
+    }
+}
