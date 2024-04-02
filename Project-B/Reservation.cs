4ae402f511b8f104e1fddf0f12dca25d849e15bd
Jasper <1056722@hr.nl>
2024-04-02 14:44:47 +0200

    Added RandomSeatReservation

--- Project-B/Reservation.cs
+++ Project-B/Reservation.cs
@@ -18,4 +18,99 @@ public class Reservation
 
         ReservationNumber = _reservationNumber;
     }
+
+    public void ReserveRandomSeat(Flight flight)
+    {
+        string SeatQual = "";
+        bool isValidInput = false;
+
+        while (!isValidInput)
+        {
+            if (flight.Airplane.Model == "Boeing 737")
+            {
+                Console.WriteLine("What seat quality:\n[1] - Economy\n[2] - Economy Extra Legroom");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    SeatQual = "Economy";
+                    isValidInput = true;
+                }
+                else if (input == "2")
+                {
+                    SeatQual = "Economy Extra Legroom";
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 1 or 2.");
+                }
+            }
+            else if (flight.Airplane.Model == "Boeing 787")
+            {
+                Console.WriteLine("What seat quality:\n[1] - Economy\n[2] - Business\n[3] - Economy Extra Legroom");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    SeatQual = "Economy";
+                    isValidInput = true;
+                }
+                else if (input == "2")
+                {
+                    SeatQual = "Business";
+                    isValidInput = true;
+                }
+                else if (input == "3")
+                {
+                    SeatQual = "Economy Extra Legroom";
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 1, 2, or 3.");
+                }
+            }
+            else if (flight.Airplane.Model == "Airbus 330")
+            {
+                Console.WriteLine("What seat quality:\n[1] - Economy\n[2] - Economy Extra Legroom\n[3] - Double seats\n[4] - Economy class in front of cabin\n[5] - Club Class");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    SeatQual = "Economy";
+                    isValidInput = true;
+                }
+                else if (input == "2")
+                {
+                    SeatQual = "Economy Extra Legroom";
+                    isValidInput = true;
+                }
+                else if (input == "3")
+                {
+                    SeatQual = "Double seats";
+                    isValidInput = true;
+                }
+                else if (input == "4")
+                {
+                    SeatQual = "Economy class in front of cabin";
+                    isValidInput = true;
+                }
+                else if (input == "5")
+                {
+                    SeatQual = "Club Class";
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 5.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid airplane model.");
+                return;
+            }
+        }
+
+        // Use SeatQual here for further processing
+        Console.WriteLine($"You have selected {SeatQual} seat.");
+    }
 }
