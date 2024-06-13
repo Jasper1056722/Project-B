ac0f5390d83fdd9595b7f389481dcba16c57829e
Jasper <1056722@hr.nl>
2024-06-13 12:25:08 +0200

    Abstract class added

---
+++ Project-B/ThirdLayer/IPerson.cs
@@ -0,0 +1,9 @@
+public abstract class IPerson
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string BirthDate { get; set; }
+    public string PhoneNumber { get; set; }
+    public string EmailAddress { get; set; }
+    public FoodPackage FoodOption { get; set; }
+}
